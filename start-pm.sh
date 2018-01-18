4b00c32b5fa9a030c7f20fbd58dc9407faddf3cf
Benny Pollak <yo372002@yahoo.com>
2018-01-18 17:14:16 -0500

    move to crypto

--- start-pm.sh
+++ start-pm.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 set -x
-/usr/local/bin/iota-pm -i http://crypto:14265 -p `nslookup home | awk '/Address[^#]*$/ {print $NF}'`:8888
+/usr/local/bin/iota-pm -i http://crypto:14265 -p `nslookup $(hostname) | awk '/Address[^#]*$/ {print $NF}'`:8888
